f10814454f4525ce85f9644000e705423773b38c
swfDaya <dayanidhi1997n@gmail.com>
2021-03-02 04:40:01 +0530

    Module 3 solution

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion.js'
+
 let quizData = require('./quiz_data.json')
 
 
@@ -9,14 +11,18 @@ class Quiz extends Component {
 
   constructor(props){
      super(props);
-     this.state = {quiz_position: 1};
+     this.state = {
+       quiz_position: 1
+     };
    }
 
 
 
   render() {
     return (
-      <div className="QuizQuestion" >{quizData.quiz_questions[0].instruction_text}</div>
+      
+
+      <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position-1]}  />
   );
   }
 }
