28deed4f5ddec4a32ee90ebb24cbf72271aa79f8
swfDaya <dayanidhi1997n@gmail.com>
2021-03-02 19:16:35 +0530

    Module 7 solution

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,9 +3,20 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
 
+  constructor(props){
+     super(props);
+     this.state = {
+       incorrectAnswer: false
+     };
+   }
+
  handleClick(buttonText) {
     if ( buttonText == this.props.quiz_question.answer ) {
-      this.props.showNextQuestionHandler()
+      this.props.showNextQuestionHandler();
+      this.setState({incorrectAnswer: false});
+    }
+    else {
+      this.setState({incorrectAnswer: true});
     }
   }
 
@@ -26,7 +37,12 @@ class QuizQuestion extends Component {
          ))}
         </ul>
        </section>
-     </main>
+       <p className='error' >
+       {
+         ( (this.state.incorrectAnswer ) ? <p>Sorry, that's not right</p> : null )
+       }
+       </p>
+      </main>
 
   );
   }
