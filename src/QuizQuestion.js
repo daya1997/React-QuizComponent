e5d09d426693e8a2f31aa267986d5e2bdc9eaa47
swfDaya <dayanidhi1997n@gmail.com>
2021-03-02 13:29:20 +0530

    Module 4 Solution

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
 
@@ -11,7 +12,7 @@ class QuizQuestion extends Component {
        </section>
        <section className="buttons">
          <ul>
-         <li>{this.props.quiz_question.answer_options}</li>
+         {this.props.quiz_question.answer_options.map(n => <QuizQuestionButton button_text={n}/>)}
          </ul>
        </section>
      </main>
